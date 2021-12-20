2385a3ac781da490385dab043cfbdb2ecec5ac9e
VGPU <k3nny1991@gmail.com>
2021-12-20 21:02:35 +0100

    Fixed Driver

--- repack.sh
+++ repack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-NVIDIA-Linux-x86_64-460.73.01-rid-vgpu-kvm/makeself.sh --bzip2 \
-    "NVIDIA-Linux-x86_64-460.73.01-rid-vgpu-kvm" \
-    ./NVIDIA-Linux-x86_64-460.73.01-grid-vgpu-kvm-v5.run \
+NVIDIA-Linux-x86_64-460.73.01-grid-vgpu-kvm/makeself.sh --bzip2 \
+    "NVIDIA-Linux-x86_64-460.73.01-grid-vgpu-kvm" \
+    ./NVIDIA-Linux-x86_64-460.73.01-grid-vgpu-kvm.run \
     "NVIDIA Accelerated Graphics Driver for Linux-x86_64 460.73.01/02 (Merged+Unlocked+Patched) Rust Version" \
     ./nvidia-installer
